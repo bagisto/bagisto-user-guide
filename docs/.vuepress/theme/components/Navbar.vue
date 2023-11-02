79e0e751c2443ed695a42c7a61b2af56443a8991
anikesh kumar <anikesh.kumar204@webkul.com>
2023-11-02 11:26:06 +0530

    Udated logo at menu

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -102,7 +102,6 @@ $navbar-horizontal-padding = 1.5rem
   a, span, img
     display inline-block
   .logo
-    height $navbarHeight - 1.4rem
     min-width $navbarHeight - 1.4rem
     margin-right 0.8rem
     vertical-align top
