5bf8ddcd225a3f17a8cddde2c03967d25e8180b1
anikeshwebkul <anikeshwebkul@users.noreply.github.com>
2023-11-02 05:58:18 +0000

    deploy: 79e0e751c2443ed695a42c7a61b2af56443a8991

--- service-worker.js
+++ service-worker.js
@@ -27,18 +27,18 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "44a78dc5b4bb262affb5a5c8aa0ecf55"
+    "revision": "8634be5951e1b2bed1110b307ab68f61"
   },
   {
-    "url": "assets/css/0.styles.fbbe2a77.css",
-    "revision": "66ff016f7409fa70fbeac58e867b4144"
+    "url": "assets/css/0.styles.d9a86543.css",
+    "revision": "6f29d0e169a044ae19181cf856824cfb"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/2.dd8691fa.js",
+    "url": "assets/js/2.ce3dec6d.js",
     "revision": "eb0cf4bbfb9b839e91727eb6d9076e53"
   },
   {
@@ -54,44 +54,44 @@ self.__precacheManifest = [
     "revision": "318b30ca5eba7b8ace88c2eedf099273"
   },
   {
-    "url": "assets/js/6.62cbcb9b.js",
-    "revision": "7972827be161ab5eeea7c0205885ff43"
+    "url": "assets/js/6.9e0b5a41.js",
+    "revision": "cc23ab1252f132849f38afbefeb1d6f7"
   },
   {
-    "url": "assets/js/7.cd1e62d2.js",
-    "revision": "c4bd390b1f071350e58a2fff7cdeffbe"
+    "url": "assets/js/7.31d480e0.js",
+    "revision": "205edb2bf65326d70be1f9d1385b5dba"
   },
   {
-    "url": "assets/js/8.207e5539.js",
-    "revision": "74d40a8ff88c414dfe9645b8d2c745a4"
+    "url": "assets/js/8.c2eed002.js",
+    "revision": "683a28cc8e100cdfe21e9021bbecc128"
   },
   {
     "url": "assets/js/9.ed09befb.js",
     "revision": "01f9a03c35d62167d081b18de33c7521"
   },
   {
-    "url": "assets/js/app.c197e812.js",
-    "revision": "e6646bd3ad5fd9ab4fa7de2552d9c762"
+    "url": "assets/js/app.0fbcc885.js",
+    "revision": "085d67aaa6d0b4ecf5bfda90fcbd7941"
   },
   {
     "url": "index.html",
-    "revision": "4e718254593d71adc6db9eee7eb8db82"
+    "revision": "ee9e4c7bf372539590ae6423467628a8"
   },
   {
     "url": "logo.png",
-    "revision": "7623b31ea8912e775aa903f3da491179"
+    "revision": "e20d14d45261a8376b1b48410c9bc1b9"
   },
   {
     "url": "prologue/contribution-guide.html",
-    "revision": "bd7cad9c23fc952e1c33cd26fd57a638"
+    "revision": "0000073e0cf412564e60378295c2ee3a"
   },
   {
     "url": "prologue/index.html",
-    "revision": "be2ab2094d6305f95c43b5195a3ccf85"
+    "revision": "630bb5db083bdd91c34610ae4babbeed"
   },
   {
     "url": "prologue/upgrade-guide.html",
-    "revision": "66ab1a41bfec8be7d476e921c26dfffa"
+    "revision": "f851cc9c227eac2f846be586a41fd78a"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
